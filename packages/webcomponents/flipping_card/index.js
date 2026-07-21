85e7fbd02cb5613a76f2849257d8b41af14d6c80
Maxim <maxtab1@mail.ru>
2026-07-22 00:14:11 +0300

    add animation from 0 to auto

--- packages/webcomponents/flipping_card/index.js
+++ packages/webcomponents/flipping_card/index.js
@@ -9,17 +9,18 @@ class FlippinCard extends HTMLElement {
                 :host {
                     display: block;
                     // width: 50px;
-                    height: min-content;
+                    // height: min-content;
                     perspective: 1000px;
                     border: 5px dashed hsl(120 50 50 / 0.5);
                     // position: relative;
+                    interpolate-size: allow-keywords;
                 }
                 .card {
                     // width: 100%;
                     // height: 100%;
                     // position: relative;
                     transform-style: preserve-3d;
-                    transition: transform 1s ease;
+                    transition: transform 2s ease;
                     display: grid;
                     grid-template-areas: "side";
                     // place-content: center;
@@ -35,10 +36,15 @@ class FlippinCard extends HTMLElement {
                     grid-area: side;
                     // position: absolute;
                     // width: 100%;
-                    // height: 100%;
+                    height: auto;
                     backface-visibility: hidden;
                     -webkit-backface-visibility: hidden;
+                    transition: height 2s;
+                    // overflow: hidden;
 
+                }
+                .side.is_hidden {
+                    height: 0px;
                 }
                 .front {
                     display: grid;
@@ -63,8 +69,18 @@ class FlippinCard extends HTMLElement {
         `;
 	}
 	flip() {
+		// console.log(this);
 		const card = this.shadowRoot.getElementById("card_wrapper");
+		const front = this.shadowRoot.querySelector(".front");
+		const back = this.shadowRoot.querySelector(".back");
+		//
 		card.classList.toggle("is_flipped");
+
+		front.classList.toggle("is_hidden", card.classList.contains("is_flipped"));
+		back.classList.toggle("is_hidden", !card.classList.contains("is_flipped"));
+
+		const active = card.classList.contains("is_flipped") ? back : front;
+		const notActive = card.classList.contains("is_flipped") ? front : back;
 	}
 }
 customElements.define("flippin-card", FlippinCard);
